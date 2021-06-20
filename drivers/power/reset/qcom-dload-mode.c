85f06514f4db2340a2bab5c6b190c3dc3ae39b81
Juhyung Park <qkrwngud825@gmail.com>
2021-06-21 00:48:27 +0900

    power: reset: qcom-dload-mode: perform a warm, nodump reboot upon a panic
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    (cherry picked from commit 7fdd49ff52e4beb5529969cd20dbc3f54bdae999)
    (cherry picked from commit ffbf65fe47302386eb9e09964a5e806a4226714c)
    (cherry picked from commit dcccd44b56dcc1c13e309f56aec8b5b4420d4df2)

--- drivers/power/reset/qcom-dload-mode.c
+++ drivers/power/reset/qcom-dload-mode.c
@@ -38,7 +38,7 @@ struct qcom_dload {
 static bool enable_dump =
 	IS_ENABLED(CONFIG_POWER_RESET_QCOM_DOWNLOAD_MODE_DEFAULT);
 static enum qcom_download_mode current_download_mode = QCOM_DOWNLOAD_NODUMP;
-static enum qcom_download_mode dump_mode = QCOM_DOWNLOAD_FULLDUMP;
+static enum qcom_download_mode dump_mode = QCOM_DOWNLOAD_MINIDUMP;
 static bool early_pcie_init_enable;
 
 static int set_download_mode(enum qcom_download_mode mode)
@@ -248,6 +248,12 @@ static int qcom_dload_panic(struct notifier_block *this, unsigned long event,
 	poweroff->in_panic = true;
 	if (enable_dump)
 		msm_enable_dump_mode(true);
+
+	// Perform a warm reboot
+	set_download_mode(QCOM_DOWNLOAD_NODUMP);
+	reboot_mode = REBOOT_WARM;
+	mb();
+
 	return NOTIFY_OK;
 }
 
@@ -259,8 +265,7 @@ static int qcom_dload_reboot(struct notifier_block *this, unsigned long event,
 						     reboot_nb);
 
 	/* Clean shutdown, disable dump mode to allow normal restart */
-	if (!poweroff->in_panic)
-		set_download_mode(QCOM_DOWNLOAD_NODUMP);
+	set_download_mode(QCOM_DOWNLOAD_NODUMP);
 
 	if (cmd) {
 		if (!strcmp(cmd, "edl")) {
